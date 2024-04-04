afd5d9c86312727e8cb5eec94dd14db282d3967a
Rodrigo <owneroxxor@gmail.com>
2024-04-04 00:06:18 -0300

    a better cleanup

--- main.go
+++ main.go
@@ -12,19 +12,23 @@ import (
 
 func main() {
 	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
 	parser := eth_parser.NewEthereumParser(ctx, nil) // Passing nil so it uses the default storage
 	cli := cli.NewCLI(ctx, parser)
 
-	// Setup signal handling
+	setupSignalHandling(cancel)
+
+	cli.Run()
+}
+
+func setupSignalHandling(cancelFunc context.CancelFunc) {
 	sigs := make(chan os.Signal, 1)
 	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
 
 	go func() {
 		<-sigs
-		cancel()
-		fmt.Println("\nreceived termination signal, exiting...")
+		fmt.Println("\nReceived termination signal, exiting...")
+		cancelFunc()
 		os.Exit(0)
 	}()
-
-	cli.Run()
 }
